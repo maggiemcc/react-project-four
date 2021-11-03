5d604427854fa05d8b56bd7a093b1f4c136cd026
Maggie McCausland <maggiemccausland@hotmail.com>
2021-11-03 13:58:17 -0600

    added links to welcome page to other pages

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,14 +1,43 @@
-import breakingBad from '../data/breakingBad.jpeg';
-// import {NavLink} from 'react-router-dom';
+import breakingBad from "../data/breakingBad.jpeg";
+import { Link } from "react-router-dom";
 
+const linkStyle = {
+  color: "white",
+  textDecoration: "none",
+  margin: "auto",
+  padding: 0,
+  textAlign: "center",
+  display: "grid", 
+  gridTemplateColumns: "1fr 1fr",
+  border: "1px solid white",
+  marginBottom: "5%",
+  borderRadius: "10px",
+  contain: "content",
+  maxWidth: 800,
+}
 const Welcome = () => {
   return (
     <div>
       <h1>Welcome To...</h1>
-      <img src={breakingBad} alt="breaking bad" style={{width: "100%",}}/>
+      <img src={breakingBad} alt="breaking bad" style={{ width: "100%", marginTop: "10px", borderTop: "2px solid white", borderBottom: "2px solid white" }} />
 
-     {/* <NavLink to="/characters" style={{color: "white", textDecoration: "none", margin: 0, padding: 0}}> <h3>Click to view all Breaking Bad characters</h3></NavLink>; */}
+    <div style={{padding: "5%"}}>
+        <Link
+          to="/characters"
+          style={linkStyle}
+        >
+          <h3 style={{margin: "auto"}}>View Breaking Bad Characters</h3>
+          <img src={"https://static1.srcdn.com/wordpress/wp-content/uploads/2020/08/Better-Call-Saul-Every-Major-Breaking-Bad-Character-Still-Missing.jpg"} alt="breaking bad" style={{ width: "100%" }} />
+        </Link>
 
+        <Link
+          to="/quotes"
+          style={linkStyle}
+        >
+          <img src={"https://i.pinimg.com/originals/70/df/18/70df18ff5cdddbe6250ac59de634b183.jpg"} alt="breaking bad quotes" style={{ width: "100%" }} />
+          <h3 style={{margin: "auto"}}>View Breaking Bad Quotes</h3>
+        </Link>
+      </div>
     </div>
   );
 };
