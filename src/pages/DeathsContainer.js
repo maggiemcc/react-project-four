12ca4e681ca5a8ba739b43ee8fac98c454e01e11
Maggie McCausland <maggiemccausland@hotmail.com>
2021-11-15 14:22:58 -0700

    made more content unavailable to see unless signed in

--- src/pages/DeathsContainer.js
+++ src/pages/DeathsContainer.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { useBreakingBadContext } from "../contexts/BreakingBadContext";
 import DeathsCard from "../components/DeathsCard";
+import { useIdentityContext } from "react-netlify-identity-gotrue";
+
 
 const DeathsContainer = () => {
   const breakingBadData = useBreakingBadContext();
+  const identity = useIdentityContext();
 
   return (
     <div>
@@ -16,16 +19,24 @@ const DeathsContainer = () => {
         }}
       >
 
-        
-        {breakingBadData.deaths.map((death) => {
-          return (
-            <DeathsCard
-              key={death.death_id}
-              death={{ ...death }}
-              sx={{ margin: "auto"}}
-            />
-          );
-        })}
+        {!identity.provisionalUser && !identity.user && (
+          <h3 style={{ color: "white" }}>Please signup or login first.</h3>
+        )};
+
+
+        {identity.user && (
+          <div>
+            {breakingBadData.deaths.map((death) => {
+              return (
+                <DeathsCard
+                  key={death.death_id}
+                  death={{ ...death }}
+                  sx={{ margin: "auto" }}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/EpisodesContainer.js
+++ src/pages/EpisodesContainer.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { useBreakingBadContext } from "../contexts/BreakingBadContext";
 import EpisodesCard from "../components/EpisodesCard";
+import { useIdentityContext } from "react-netlify-identity-gotrue";
+
 
 const EpisodesContainer = () => {
   const breakingBadData = useBreakingBadContext();
+  const identity = useIdentityContext();
 
   return (
     <div>
@@ -16,16 +19,24 @@ const EpisodesContainer = () => {
         }}
       >
 
-        
-        {breakingBadData.episodes.map((episode) => {
-          return (
-            <EpisodesCard
-              key={episode.episode_id}
-              episode={{ ...episode }}
-              sx={{ margin: "auto"}}
-            />
-          );
-        })}
+        {!identity.provisionalUser && !identity.user && (
+          <h3 style={{ color: "white" }}>Please signup or login first.</h3>
+        )};
+
+        {identity.user && (
+          <div style={{margin: 0}}>
+            {breakingBadData.episodes.map((episode) => {
+              return (
+                <EpisodesCard
+                  key={episode.episode_id}
+                  episode={{ ...episode }}
+                  sx={{ margin: "auto" }}
+                />
+              );
+            })}
+          </div>
+
+        )}
       </div>
     </div>
   );
